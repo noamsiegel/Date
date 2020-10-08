cfc7a8012c788ddb5a9ad4d50e1b691edc802cb1
Noam Siegel <noamsiegel99@gmail.com>
2020-10-08 04:38:07 +0000

    Initial commit

---
+++ Date.java
@@ -0,0 +1,24 @@
+// Noam Siegel
+// noamsiegel@my.smccd.edu
+// CIS 254 OL
+// Date.java
+// Present date in American and European output
+// Assignment 2
+// Sep 16, 2020
+// I have not cheated or plagiarized on this program.
+
+public class Date {
+
+  public static void main(String[] args)  {
+    
+     String day = "Wednesday";
+     String date = "16";
+     String month = "September";
+     String year = "2020";
+
+     // Display result
+     System.out.print("Noam Siegel");
+     System.out.print('\n'+"American format: " + day + ", " + month + " "+ date + ", " + year);
+     System.out.print('\n'+"European format: " + day + " " + date + " " + month + " " + year);  
+  }
+}
